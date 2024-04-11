5db339a8352628a36d4a7185c2b748d1f196a1ac
ThatRandomSpbuStudent <sokhanivan@yandex.ru>
2024-04-11 20:41:52 +0300

    Added basic class and constructor

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "trinary.h"
+
+int main() {
+  trinary<32> trit(12.12f);
+  trit.draw();
+  return 0;
+}
---
+++ src/trinary.h
@@ -0,0 +1,85 @@
+#ifndef TRINARY
+#define TRINARY
+
+#include <cmath>
+#include <vector>
+
+template <size_t seq_size>
+
+class trinary {
+ private:
+  std::vector<unsigned char> seq;
+  std::vector<unsigned char> frac_seq;
+
+ public:
+  void draw() {
+    for (int i = 0; i < seq_size; i++) {
+      std::cout << (int)(seq[i]);
+    }
+    std::cout << ".";
+    for (int i = 0; i < seq_size; i++) {
+      std::cout << (int)(frac_seq[i]);
+    }
+    std::cout << "\n";
+  }
+  template <typename B>
+  trinary(B number = 0.0) {
+    seq.reserve(seq_size);
+    frac_seq.reserve(seq_size);
+    std::cout << sizeof(B) << '\n';
+    if (pow(2, 8 * sizeof(B)) > pow(3, seq_size)) {
+      throw std::logic_error("Sequence size too small for the number!!");
+    }
+    bool sign = (number < 0);
+    unsigned long long whole_part =
+        abs(static_cast<unsigned long long>(number / 1));
+    unsigned long long pow_10 = pow(10, seq_size);
+    unsigned long long frac_part =
+        static_cast<unsigned long long>((number - whole_part) * pow_10 / 10);
+    std::vector<short> temp_seq(seq_size);
+    int j = 0;
+    while (whole_part > 0) {
+      short temp = whole_part % 3;
+      whole_part = whole_part / 3;
+      if (temp == 2) {
+        whole_part++;
+      }
+      temp_seq[seq_size - j - 1] = (temp == 0 ? 0 : (temp == 1) ? 1 : 2);
+      j++;
+    }
+    for (int x = seq_size - 1; x > -1; x--) {
+      if (sign) {
+        if ((temp_seq[x]) == 2) {
+          seq[x] = static_cast<unsigned char>(1);
+        } else if (temp_seq[x] == 1) {
+          seq[x] = static_cast<unsigned char>(2);
+        } else {
+          seq[x] = static_cast<unsigned char>(0);
+        }
+      } else {
+        seq[x] = static_cast<unsigned char>(temp_seq[x]);
+      }
+    }
+    int accuracy_iterator = 0;
+    short left = 0;
+    while (accuracy_iterator < seq_size) {
+      frac_part *= 3;
+      short temp = frac_part / (pow_10 / 10) + left;
+      if (temp >= 2) {
+        left = 1;
+      } else {
+        left = 0;
+      }
+      if (frac_part > pow_10 / 10) {
+        frac_part %= (pow_10 / 10);
+      }
+      frac_seq.push_back(static_cast<unsigned char>(temp == 0     ? 0
+                                                    : (temp == 1) ? 1
+                                                    : (temp == 2) ? 2
+                                                                  : 0));
+      accuracy_iterator++;
+    }
+  }
+};
+
+#endif
